047a4a0abdfe50694644c9a20da344ed03b548f9
Ritika Rana <ranaritu414@gmail.com>
2023-04-14 23:40:28 +0530

    Add files via upload

---
+++ largestofthreenum.java
@@ -0,0 +1,27 @@
+
+import java.util.*;
+public class largestofthreenum {
+    public static void main (String args[]){
+    
+
+         int Ritika =1000000 , Rohan=300000, Anamika=400000;
+
+         if
+         ((Ritika>=Rohan)&&(Ritika>=Anamika))
+         {
+         System.out.println("Largest is Ritika");
+         }
+        else if
+         
+         (Rohan>=Anamika)
+        {
+         System.out.println("largest is Rohan");
+        }
+        else
+        {
+         System.out.println("largest is Anamika");
+        }
+    }
+
+    
+}
