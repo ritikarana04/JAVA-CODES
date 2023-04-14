523452cd4f906bbb288e3b066bf9baec8c671457
Ritika Rana <ranaritu414@gmail.com>
2023-04-15 00:18:32 +0530

    Add files via upload

---
+++ Switchstatement.java
@@ -0,0 +1,18 @@
+public class Switchstatement {
+    public static void main(String args[]){
+
+int num=1;
+switch(num){
+
+    case 1: System.out.println("chole bhature ");
+break;
+    case 2: System.out.println(" manchurian");
+break;
+    case 3: System.out.println(" lassi");
+break;
+}
+}
+
+}
+        
+
