5ad843b835a2e76473b4af736898fd31f8dc46dd
Ritika Rana <ranaritu414@gmail.com>
2023-04-14 23:55:28 +0530

    Add files via upload

---
+++ teranaryopeartor.java
@@ -0,0 +1,12 @@
+public class teranaryopeartor {
+public static void main(String args[]){
+ int num =5
+ ;
+//ternary operator
+
+String type=((num%2)==0)?"even":"odd";
+System.out.println(type);
+
+
+}
+}
