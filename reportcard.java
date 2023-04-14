986bf20e931be7525babe03aba0548f93b7258bd
Ritika Rana <ranaritu414@gmail.com>
2023-04-15 00:05:12 +0530

    Add files via upload

---
+++ reportcard.java
@@ -0,0 +1,10 @@
+public class reportcard {
+    public static void main(String args[]){
+        int marks=32;
+ String result = marks>=33? "pass":"fail";
+ System.out.println(result);
+
+
+    }
+    
+}
