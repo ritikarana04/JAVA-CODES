b3dcd2bf8c46ebcd2afc49f3e0cb0503c9ebceed
Ritika Rana <ranaritu414@gmail.com>
2023-04-18 16:16:06 +0530

    Add files via upload

---
+++ whileloop.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class whileloop{
+    public static void main(String args[]){
+int counter=0;
+while(counter<1000){
+System.out.println("Hello World");
+counter++;
+}
+System.out.println("printed HW 10x");
+
+}
+
+
+
+
+    }
