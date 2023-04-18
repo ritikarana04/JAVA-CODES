13e07c4616e7f5c7f1537c9c588fe70f6a705aca
Ritika Rana <ranaritu414@gmail.com>
2023-04-18 18:34:17 +0530

    Add files via upload

---
+++ rev.java
@@ -0,0 +1,19 @@
+public class rev{
+public static void main(String args[]){
+int n=20012211;
+int rev=0;
+
+
+while(n>0){
+
+    int lastDigit=n%10;
+    rev=(rev*10)+lastDigit;
+    n=n/10;
+
+}
+System.out.println(rev);
+}
+
+
+
+}
