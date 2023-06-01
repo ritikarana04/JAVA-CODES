b7de5fdbb8efd25a5cfab7a192558f36b6a62d6e
Ritika Rana <ranaritu414@gmail.com>
2023-06-01 11:43:35 +0530

    Add files via upload

---
+++ array/array9.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class array9 {
+    public static void maxSubarraysSum(int numbers[]) {
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+
+        for (int i = 0; i < numbers.length; i++) {
+            int start = i;
+
+            for (int j = i; j < numbers.length; j++) {
+                int end = j;
+                currSum = 0;
+
+                for (int k = start; k <= end; k++) {// print
+                    System.out.println(numbers[k] + " ");// subarrays
+                    // subarray sum
+
+                    currSum += numbers[k];
+                }
+
+                System.out.println(currSum);
+                if (maxSum < currSum) {
+                    maxSum = currSum;
+                }
+
+            }
+
+        }
+        System.out.println("max sum = " + maxSum);
+    }
+
+    public static void main(String args[]) {
+        int numbers[] = { 1, -2, 6, -1, 3 };
+        maxSubarraysSum(numbers);
+    }
+}
+
+// time complexity O(n^3)
