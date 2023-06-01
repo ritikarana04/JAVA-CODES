49ec884bc91f7c9bbf07bd3d11de8345f2c787f7
Ritika Rana <ranaritu414@gmail.com>
2023-06-01 16:36:32 +0530

    Create selection_sort.java

---
+++ Sorting/selection_sort.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+public class selection_sort {
+
+    public static void bubbleSort(int arr[]) {
+
+        for (int turn = 0; turn < arr.length - 1; turn++) {
+
+            for (int j = 0; j < arr.length - 1 - turn; j++) {
+                if (arr[j] > arr[j + 1]) {
+
+                    // swap
+
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+                }
+
+            }
+
+        }
+
+    }
+
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+
+        }
+        System.out.println();
+    }
+
+    public static void selectionSort(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minPos = i;
+
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[minPos] > arr[j]) {
+                    minPos = j;
+
+                }
+
+            }
+            // swap
+            int temp = arr[minPos];
+            arr[minPos] = arr[i];
+            arr[i] = temp;
+
+        }
+
+    }
+
+    public static void main(String args[]) {
+        int arr[] = { 5, 4, 3, 2, 1 };
+        selectionSort(arr);
+        printArr(arr);
+    }
+}
+// time complexity O(n^2)
