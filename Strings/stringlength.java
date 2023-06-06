bac8769e66c397f9e7ddfcb994d6fbef2e809784
Ritika Rana <ranaritu414@gmail.com>
2023-06-06 18:54:54 +0530

    Create stringlength.java

---
+++ Strings/stringlength.java
@@ -0,0 +1,10 @@
+public class stringlength {
+
+    public static void main(String args[]) {
+
+        String fullName = "ony yours Ritika Rana";
+        System.out.println(fullName.length());
+
+    }
+
+}
