ab256dcfbdfb54aaf88219234a304f58c6309fc5
Ritika Rana <ranaritu414@gmail.com>
2023-06-12 15:44:59 +0530

    Add files via upload

---
+++ Strings/substring.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+
+public class substring {
+
+    public static String substring(String str, int si, int ei) {
+        String substr = "";
+        for (int i = si; i < ei; i++) {
+            substr += str.charAt(i);
+
+        }
+        return substr;
+
+    }
+
+    public static void main(String args[]) {
+        // substring
+        String str = "Hello World";
+        System.out.println(substring(str, 0, 5));
+
+    }
+}
