e5da3d3ed46ada42903b2753264609a12c8f20ca
Ritika Rana <ranaritu414@gmail.com>
2023-07-28 00:26:03 +0530

    Create Classroom.java

---
+++ arrayList/Classroom.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Classroom {
+
+    public static void main(String args[]) {
+
+        // classNAme objectNAme= new ClassName();
+        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<String> list2 = new ArrayList<>();
+        ArrayList<Boolean> list3 = new ArrayList<>();
+
+    }
+}
