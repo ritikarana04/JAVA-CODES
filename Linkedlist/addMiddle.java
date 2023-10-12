c708253eae9abaf1bf0e4cf6649b1c4484205ad4
Ritika Rana <ranaritu414@gmail.com>
2023-10-13 00:15:05 +0530

    Create addMiddle.java

---
+++ Linkedlist/addMiddle.java
@@ -0,0 +1,84 @@
+import java.util.*;
+
+public class LinkedList {
+
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public Node head;
+    public Node tail;
+
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+    }
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+
+    public void print() {
+        if (head == null) {
+            System.out.println("LL is empty");
+            return;
+        }
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public void add(int idx, int data) {
+        Node newNode = new Node(data);
+        Node temp = head;
+        int i = 0;
+        while (i < idx - 1) {
+            temp = head;
+            i++;
+
+        }
+        // i =idx-1; temp->prev
+        newNode.next = temp.next;
+        temp.next = newNode;
+
+    }
+
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+     
+
+           ll.addFirst(2);
+
+    ll.addFirst(1);
+        
+
+        ll.addLast(3);
+       
+        ll.addLast(4);
+
+    
+        ll.add(2,9);
+ 
+        ll.print();
+    }
+}
